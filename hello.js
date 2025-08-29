4c96eee96338184834b249da303e9f9d0c972ba3
BrainPreserve <ggms.1361@gmail.com>
2025-08-29 13:19:42 -0400

    Delete hello.js

--- hello.js
+++
@@ -1,8 +0,0 @@
-// /netlify/functions/hello.js
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ok: true, msg: 'hello from Netlify Functions' })
-  };
-};
